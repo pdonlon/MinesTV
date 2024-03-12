196c4a3aedd50a88d862bcab0e9f9ebb5d2a7837
Philip Donlon <pdonlon57@gmail.com>
2024-03-11 23:45:34 -0400

    Initial Commit

---
+++ Mines/MinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesApp.swift
+//  Mines
+//
+//  Created by Philip Donlon on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
